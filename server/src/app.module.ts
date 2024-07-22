0cec2ad3a46a9efd29a57fde9becdfe5a630219d
Jinyshin <iwbm312@gmail.com>
2024-07-23 02:43:55 +0900

    feat: addd modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GroupsModule } from './groups/groups.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { ParticipationsModule } from './participations/participations.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [GroupsModule],
+  imports: [
+    GroupsModule,
+    AppointmentsModule,
+    ParticipationsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
