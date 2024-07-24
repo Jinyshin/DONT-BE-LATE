051a3ec414ea053de4350e14c13554f9d63fea63
Jinyshin <iwbm312@gmail.com>
2024-07-24 17:48:47 +0900

    feat: add appointment dto

--- server/src/appointments/dto/create-appointment.dto.ts
+++ server/src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,29 @@
-export class CreateAppointmentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsDateString, IsNumber } from 'class-validator';
+
+export class CreateAppointmentDto {
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  gid: number;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @ApiProperty()
+  @IsDateString()
+  @IsNotEmpty()
+  meet_at: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  penalty: string;
+}
