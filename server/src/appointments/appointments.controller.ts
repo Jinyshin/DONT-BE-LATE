939739a3fe76b68cfe96a6faca055781629c343e
Jinyshin <iwbm312@gmail.com>
2024-07-24 17:51:05 +0900

    feat: appointment create

--- server/src/appointments/appointments.controller.ts
+++ server/src/appointments/appointments.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/v1/appointments')
 @ApiTags('Appointments')
@@ -9,16 +9,21 @@ export class AppointmentsController {
   constructor(private readonly appointmentsService: AppointmentsService) {}
 
   @Post()
+  @ApiOperation({ summary: '새 약속 생성' })
   create(@Body() createAppointmentDto: CreateAppointmentDto) {
-    return this.appointmentsService.create(createAppointmentDto);
+    console.log('됨?');
+    // TODO: JWT에서 userId 추출해서 전달하기
+    return this.appointmentsService.create(createAppointmentDto, 1);
   }
 
   @Get()
+  @ApiOperation({ summary: '전체 약속 목록 조회' })
   findAll() {
     return this.appointmentsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '약속 상세 조회' })
   findOne(@Param('id') id: string) {
     return this.appointmentsService.findOne(+id);
   }
--- server/src/appointments/appointments.service.ts
+++ server/src/appointments/appointments.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AppointmentsService {
-  create(createAppointmentDto: CreateAppointmentDto) {
-    return 'This action adds a new appointment';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createAppointmentDto: CreateAppointmentDto, userId: number) {
+    // 약속 엔티티 생성 및 저장
+    const appointment = await this.prisma.appointments.create({
+      data: {
+        ...createAppointmentDto,
+        meet_at: new Date(createAppointmentDto.meet_at),
+        is_deleted: false,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+
+    // Participants 테이블에 현재 user 추가
+    await this.prisma.participants.create({
+      data: {
+        aid: appointment.id,
+        uid: userId,
+        is_deleted: false,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+
+    return appointment;
   }
 
   findAll() {
