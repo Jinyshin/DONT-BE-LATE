f8e7f0e325eb495675d0634458c3a23e36ba5ffe
ys126800 <yunseopark@kaist.ac.kr>
2024-10-28 04:18:11 +0900

    code rabbit rivew was applied

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@
 importScripts('https://www.gstatic.com/firebasejs/9.17.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.17.2/firebase-messaging-compat.js');
 
-let messaging;
+let messaging=null;
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SET_ENV') {
@@ -15,14 +15,15 @@ self.addEventListener('message', (event) => {
       appId: event.data.appId,
     };
 
-    // Firebase 초기화
-    firebase.initializeApp(firebaseConfig);
-
-    // Firebase Messaging 초기화
-    messaging = firebase.messaging();
-
-
-
+    try {
+      // Firebase 초기화
+      firebase.initializeApp(firebaseConfig);
+      
+      // Firebase Messaging 초기화
+      messaging = firebase.messaging();
+    } catch (error) {
+      console.error('Firebase 초기화 중 오류 발생:', error);
+    }
 
     // 백그라운드 메시지 처리
     messaging.onBackgroundMessage(function (payload) {
@@ -31,7 +32,7 @@ self.addEventListener('message', (event) => {
       const notificationTitle = payload.notification.title;
       const notificationOptions = {
         body: payload.notification.body,
-        icon: '/icon-16*16.png',  // 푸시 알림 아이콘 설정
+        icon: '/icon-16x16.png',  // 푸시 알림 아이콘 설정
       };
 
       self.registration.showNotification(notificationTitle, notificationOptions);
