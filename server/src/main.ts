f0ae960abab329db071c56a0f025d289e6d5175c
박상우 <parks@Parks-Mac.local>
2024-11-06 19:24:36 +0900

    tmp 2

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
-import { AppointmentsModule } from './appointments/appointments.module';
-import { AppointmentsService } from './appointments/appointments.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { initializeApp } from 'firebase-admin';
 
 declare const module: any;
 dotenv.config();
+
+const firebaseApp = initializeApp();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -28,18 +31,54 @@ async function bootstrap() {
 
   // 약속 push 알림 관련 백그라운드 작업
   setInterval(async () => {
-    const appointmentService = app.select(AppointmentsModule)
-                                  .get(AppointmentsService, { strict: true });
-    const upcomings = await appointmentService.listUpcomings();
+    const now = new Date(Date.now());
+    const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
+    const prismaService = app.select(PrismaModule)
+                              .get(PrismaService, { strict: true });
 
-    for (const appointment of upcomings) {
-      const now = Date.now();
-      const { title, meet_at, users } = appointment;
+    const upcomings = await prismaService.appointment.findMany({
+      select: {
+        title: true,
+        meet_at: true,
+        users: {
+          select: {
+            user: {
+              select: {
+                fcm_tokens: {
+                  where: {
+                    expires_at: { gt: now }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      where: {
+        AND: [
+          { meet_at: { gt: now } },
+          { meet_at: { lte: oneHourLater } }
+        ],
+        is_deleted: false
+      }
+    });
 
-      setTimeout(() => {
-        // TODO!
-        // send noti
-      }, meet_at.getTime() - now);
+    for (const { title, meet_at, users } of upcomings) {
+      for (const { user: { fcm_tokens } } of users) {
+        for (const { token } of fcm_tokens) {
+          setTimeout(() => {
+            // TODO!
+            // send push using firebase api
+            firebaseApp.messaging().send({
+              notification: {
+                title: ``,
+                body: ``
+              },
+              condition: ''
+            });
+          }, now.getTime() - meet_at.getTime());
+        }
+      }
     }
   }, 60 * 60 * 1000);
 }
