3681e95faa4b3602668e6648e6eb5344d03a85b8
Jinyshin <iwbm312@gmail.com>
2024-11-17 11:52:24 +0900

    fix: prisma, jwtmodule 추가

--- server/src/notifications/notifications.module.ts
+++ server/src/notifications/notifications.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: +process.env.JWT_MAX_AGE },
+    }),
+  ],
   providers: [NotificationsService],
-  controllers: [NotificationsController]
+  controllers: [NotificationsController],
 })
 export class NotificationsModule {}
