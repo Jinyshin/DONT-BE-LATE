4081d2c450ec1b8de67a7db0dac5134626ab3f0a
Jinyshin <iwbm312@gmail.com>
2025-02-01 20:34:45 +0900

    feat: JWT Guards 적용

---
+++ server/src/accounts/guards/jwt.guard.ts
@@ -0,0 +1,29 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1]; // Bearer token 추출
+
+    if (!token) {
+      throw new UnauthorizedException('JWT token is missing');
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      request.user = decoded;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid JWT token');
+    }
+  }
+}
--- server/src/appointments/appointments.controller.ts
+++ server/src/appointments/appointments.controller.ts
@@ -1,23 +1,25 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Param,
-  Headers,
+  Controller,
   Delete,
-  Patch,
-  UnauthorizedException,
+  Get,
+  Headers,
+  Param,
   ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { AppointmentsService } from './appointments.service';
-import { CreateAppointmentDto } from './dto/create-appointment.dto';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { PatchAppointmentDto } from './dto/patch-appointment.dto';
-import { authorize } from 'src/utils/jwt-auth';
 import { JwtService } from '@nestjs/jwt';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/accounts/guards/jwt.guard';
+import { authorize } from 'src/utils/jwt-auth';
+import { AppointmentsService } from './appointments.service';
 import { CheckinResponseDto } from './dto/checkin-response.dto';
+import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { GetAppointmentDetailDto } from './dto/get-appointment-detail.dto';
+import { PatchAppointmentDto } from './dto/patch-appointment.dto';
 
 @Controller('api/v1/appointments')
 @ApiTags('Appointments')
@@ -28,9 +30,12 @@ export class AppointmentsController {
   ) {}
 
   @Post()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '새 약속 생성' })
-  create(@Body() createAppointmentDto: CreateAppointmentDto) {
-    return this.appointmentsService.create(createAppointmentDto, 1);
+  create(@Body() createAppointmentDto: CreateAppointmentDto, @Req() req) {
+    const userId = req.user.id;
+    return this.appointmentsService.create(createAppointmentDto, userId);
   }
 
   @Post(':id/checkin')
@@ -42,24 +47,6 @@ export class AppointmentsController {
     const { id: uid } = await authorize(this.jwtService, authorization);
     return this.appointmentsService.createCheckin(id, uid);
   }
-  // async createCheckin(
-  //   @Param('id') id: number,
-  //   @Headers('Authorization') token: string,
-  // ): Promise<Checkin> {
-  //   if (!token) {
-  //     throw new UnauthorizedException('No token provided');
-  //   }
-
-  //   let userId: number;
-  //   try {
-  //     const decoded = this.jwtService.verify(token.replace('Bearer ', ''));
-  //     userId = decoded.userId;
-  //   } catch (error) {
-  //     throw new UnauthorizedException('Invalid token');
-  //   }
-
-  //   return this.appointmentsService.createCheckin(aid, userId);
-  // }
 
   @Get()
   @ApiOperation({ summary: 'userId를 통해 메인에 띄울 약속 받아오기' })
@@ -67,21 +54,6 @@ export class AppointmentsController {
     const { id: uid } = await authorize(this.jwtService, authorization);
     return this.appointmentsService.getMyAppointments(uid);
   }
-  // async getMyAppointments(@Headers('Authorization') token: string) {
-  //   if (!token) {
-  //     throw new UnauthorizedException('No token provided');
-  //   }
-
-  //   let userId: number;
-  //   try {
-  //     const decoded = this.jwtService.verify(token.replace('Bearer ', ''));
-  //     userId = decoded.userId;
-  //   } catch (error) {
-  //     throw new UnauthorizedException('Invalid token');
-  //   }
-
-  //   return this.appointmentsService.getMyAppointments(userId);
-  // }
 
   @Get(':aid')
   @ApiOperation({ summary: '약속 상세 조회' })
